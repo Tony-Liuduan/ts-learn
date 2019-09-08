15546ad3bb629ee6f5d9ad615dae87168960bb22
liuduan <liuduan.05.05@163.com>
2019-09-08 16:51:29 +0800

    搭建基本环境 ts + webpack

--- src/web/components/Home/index.tsx
+++ src/web/components/Home/index.tsx
@@ -1,53 +1,12 @@
 import * as React from "react";
-import HooksRedux from "./HooksRedux";
-
-
-const {
-    Provider,
-    store,
-} = HooksRedux({
-    initalState: {
-        name: "ts-learn",
-        age: 18,
-    }
-});
-
-
-const actionAdd = () => {
-    return {
-        type: "init",
-    }
-}
-
-const Button = () => {
-    const handleAdd = () => {
-        store.dispath(actionAdd);
-    };
-
-    return <button onClick={handleAdd}>新增</button>
-};
-
 
 const Home = () => {
-    const state = store.useContext();
     return (
         <div>
             It is home component
-            {state.age}
-            <hr />
-            <Button />
         </div>
     );
 };
 
 
-const WarpHome = () => {
-    return (
-        <Provider>
-            <Home />
-        </Provider>
-    );
-}
-
-
-export default WarpHome;
+export default Home;
